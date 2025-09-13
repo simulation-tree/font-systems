1fdd5914ec63c01f922774382fcec8dd411536cb
Phill <philldamaskin@gmail.com>
2025-09-13 17:20:15 -0400

    updated worlds api used

--- source/Systems/FontImportSystem.cs
+++ source/Systems/FontImportSystem.cs
@@ -64,7 +64,7 @@ namespace Fonts.Systems
             for (int c = 0; c < chunks.Length; c++)
             {
                 Chunk chunk = chunks[c];
-                if (chunk.Definition.ContainsComponent(requestType))
+                if (chunk.componentTypes.Contains(requestType))
                 {
                     ReadOnlySpan<uint> entities = chunk.Entities;
                     ComponentEnumerator<IsFontRequest> components = chunk.GetComponents<IsFontRequest>(requestType);
